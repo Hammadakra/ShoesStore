9087de79fc4061a406f28f95594a3be88303728b
Hammadakra <41269248+Hammadakra@users.noreply.github.com>
2020-08-04 14:13:08 -0700

    upadate
    in this chunk i did work with react router for making a link between pages

---
+++ shoes-store/src/Components/product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function product()
+{
+    return (
+        <div>
+            <h1>Welcome to Product</h1>
+        </div>
+    )
+}
+
+export default product;
---
+++ shoes-store/src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Home from './Components/home';
+import contact from './Components/contact';
+import product from './Components/product';
+import NavBar from './Components/NavBar';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+
+} from 'react-router-dom';
+export * from "react-router";
+
+function RouterConfig()
+{
+    return(
+        <div>
+            
+            <Router>
+                <NavBar/>
+                <switch>
+                <Route exact path ="/" component={Home}/> 
+                <Route path ="/contact" component={contact}/> 
+                <Route path ="/product" component={product}/> 
+                </switch>          
+            </Router>
+        </div>
+    )
+}
+
+export default RouterConfig;
