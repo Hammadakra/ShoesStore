9a42d87402f314c2c3f918580f5d40fd2fcc5078
Hammadakra <41269248+Hammadakra@users.noreply.github.com>
2020-08-10 13:46:09 -0700

    update

--- shoes-store/src/Components/NavBar.js
+++ shoes-store/src/Components/NavBar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import './App.css';
-
+import bgTile from './blackLogo.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Navbar()
 {
     return(
     <div>
-        <div className='Nav'>
+         <div className="header">
+       
+       
+        <div className=' navbar navbar-expand-sm navbar-dark px-sm-5'>
+        <Link to='/'><img id="logo" src={bgTile} /></Link>
         <Link to='/' >Home</Link>
         <Link to='/contact'>about</Link>
         <Link to='/product'>product</Link>
-        </div>
+        </div></div>
     </div>
     );
 }
--- shoes-store/src/Header.js
+++ shoes-store/src/Header.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import bgTile from './logo.jpg'
 
 function Header()
 {
-    return(
-         <img src={bgTile}/>
-        ); 
+ return
 }
 
 export default Header;
